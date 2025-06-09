8ef31ff4c0ab73603978c63ac721b53c75e51de5
Ganesh Mane <ganeshashokmane95@gmail.com>
2025-06-09 18:41:00 +0530

    Create LeapYearChecker.java

---
+++ LeapYearChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        boolean isLeap = false;
+
+        if (year % 4 == 0 ) {
+            isLeap = true;
+        }
+        if (isLeap) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is Not a Leap Year.");
+        }
+    }
+}
