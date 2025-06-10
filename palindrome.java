47ebbf97ffdd736491d44e80278af4d736ae5a40
Ganesh Mane <ganeshashokmane95@gmail.com>
2025-06-10 12:15:04 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
 class palindrome {
     public static void main(String[] args) {
-        System.out.println("Enter Name");
+        System.out.print("Enter Name : ");
         Scanner sc=new Scanner(System.in);
         String A = sc.nextLine();
         boolean isPalindrome = true;
-
         int len = A.length();
 
-        for (int i = 0; i < len/2; i++) {
-            
+        for (int i = 0; i < len; i++) {
                 if (A.charAt(i) != A.charAt(len - 1 - i)) {
                 isPalindrome = false;
-                break;
-            
             }
         }
-
         if (isPalindrome) {
             System.out.println(A + " is a Palindrome.");
         } else {
